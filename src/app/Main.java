4d84ae88013b2def5df8bbbb8aed1b08cd1a33fb
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-11-02 16:02:04 +0200

    Change Main due new app flow.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,16 +1,11 @@
 package app;
 
-import app.model.DataHandler;
-import app.model.MyThread;
+import app.controller.MainAppController;
 
 public class Main {
 
     public static void main(String[] args) {
-        DataHandler dataHandler = new DataHandler();
-        MyThread myThread1 = new MyThread("Thread 1", dataHandler);
-        MyThread myThread2 = new MyThread("Thread 2", dataHandler);
-        myThread1.start();
-        myThread2.start();
+        MainAppController.start();
     }
 }
 
